6d3ce1e4e30b9aea191ecf980aee86cf9cab21c4
kamarat <kamarat@centrum.cz>
2017-03-24 09:36:35 +0100

    Úprava komentárov.

--- iButton_to_Wiegand.ino
+++ iButton_to_Wiegand.ino
@@ -1,73 +1,69 @@
 /*== iButton to Wiegand ==
-* ================================
-*
-* iButton_to_Wiegand.ino - v3
-*
-* Program nacita ID z iButtona a vytvori signal v protokole Wiegand
-*
-* @author: mr.nobody
-* @date:   marec 2016 - januar 2017
-*
-* mr.nobody (cleft) 2016
-*
-* v2 - marec 2016
-* v3 - januar 2017
-*/
+ *========================
+ *
+ * iButton_to_Wiegand.ino
+ *
+ * Program nacita ID z iButtona a vytvori signal v protokole Wiegand
+ *
+ * @author: kamarat
+ * @date:   marec 2016 - januar 2017
+ * 
+ * @version: [2.0.0] - marec 2017
+ *
+ * kamarat (cleft) 2016 - 2017
+ *
+ */
 
 /*== ZAPOJENIE PINOV ==
-* =====================
-*
-* ARDUINO --- PERIFERIE
-* A   0 -
-*
-* D  10 - kontakt TM citacky 1 iButton
-* D  11 - kontakt TM citacky 2 iButton
-* D  12 - kontakt TM citacky 3 iButton
-* D  13 - kontakt TM citacky 4 iButton
-*
-* D   2 - vystup Wiegand 1 DATA0
-* D   3 - vystup Wiegand 1 DATA1
-* D   4 - vystup Wiegand 2 DATA0
-* D   5 - vystup Wiegand 2 DATA1
-* D   6 - vystup Wiegand 3 DATA0
-* D   7 - vystup Wiegand 3 DATA1
-* D   8 - vystup Wiegand 4 DATA0
-* D   9 - vystup Wiegand 4 DATA1
-*
-* Rezistor 2k2 - medzi D10 a 5V
-* Rezistor 2k2 - medzi D11 a 5V
-* Rezistor 2k2 - medzi D12 a 5V
-* Rezistor 2k2 - medzi D13 a 5V
-*/
+ *=====================
+ *
+ * ARDUINO --- PERIFERIE
+ * A   0 -
+ *
+ * D  10 - kontakt TM citacky 1 iButton
+ * D  11 - kontakt TM citacky 2 iButton
+ * D  12 - kontakt TM citacky 3 iButton
+ * D  13 - kontakt TM citacky 4 iButton
+ *
+ * D   2 - vystup Wiegand 1 DATA0
+ * D   3 - vystup Wiegand 1 DATA1
+ * D   4 - vystup Wiegand 2 DATA0
+ * D   5 - vystup Wiegand 2 DATA1
+ * D   6 - vystup Wiegand 3 DATA0
+ * D   7 - vystup Wiegand 3 DATA1
+ * D   8 - vystup Wiegand 4 DATA0
+ * D   9 - vystup Wiegand 4 DATA1
+ *
+ * Rezistor 2k2 - medzi D10 a 5V
+ * Rezistor 2k2 - medzi D11 a 5V
+ * Rezistor 2k2 - medzi D12 a 5V
+ * Rezistor 2k2 - medzi D13 a 5V
+ */
 
 /*== KNIZNICE A SUBORY ==
-* =======================
-*/
+ *=======================
+ */
 #include <OneWire.h>
 
 /*== GLOBALNE PREMENNE ==
-* =======================
-* premenne popisujuce zapojene piny
-* premenne popisujuce nastavenie Arduina
-*/
+ *=======================
+ */
 
 // Definicia odladovania a vypisov
 #define DEBUG 1
 
 /*== Deklaracia konstant ==
-*/
+ */
 const uint8_t WIEGAND = 26;       // nastavenie protokolu
 const uint8_t POCET_CITACIEK = 2;
 
-// analogove piny
-
-// digitalne piny
 // pripojenie citaciek na digitalne piny
 const uint8_t CITACKA1 = 10;
 const uint8_t CITACKA2 = 11;
 const uint8_t CITACKA3 = 12;
 const uint8_t CITACKA4 = 13;
 const uint8_t CITACKY[] = { CITACKA1, CITACKA2, CITACKA3, CITACKA4 };
+
 // zapojenie Wiegand vystupov
 const uint8_t W1_DATA0 = 2;
 const uint8_t W1_DATA1 = 3;
@@ -80,7 +76,7 @@ const uint8_t W4_DATA1 = 9;
 const uint8_t W_VYSTUPY[] = { W1_DATA0, W1_DATA1, W2_DATA0, W2_DATA1, W3_DATA0, W3_DATA1, W4_DATA0, W4_DATA1 };
 
 /*== Deklaracia premennych ==
-*/
+ */
 OneWire dsButton[ 4 ] = {
   OneWire( CITACKA1 ),
   OneWire( CITACKA2 ),
@@ -95,8 +91,8 @@ uint8_t citacka;          // aktivna citacka
 String keyStatus = "";
 
 /*== INICIALIZACIA ==
-* ===================
-*/
+ *===================
+ */
 void setup()
 {
   // Nastavenie datovych pinov pre vystup Wiegand
@@ -105,7 +101,7 @@ void setup()
     digitalWrite( i, HIGH );
   }
 
-  Serial.begin(57600);  // Inicializacia serioveho vystupu
+  Serial.begin( 57600 );  // Inicializacia serioveho vystupu
   #if DEBUG == 1
     Serial.println( F( "Inicializacia ukoncena." ));
   #endif
@@ -113,8 +109,8 @@ void setup()
 }
 
 /*== HLAVNY PROGRAM ==
-* ====================
-*/
+ *====================
+ */
 void loop()
 {
   for ( citacka = 0; citacka < POCET_CITACIEK; citacka++ ) {
@@ -164,9 +160,9 @@ void loop()
   }
 }
 
-/*== DEFINICIA FUNKCII ==
-* =======================
-*/
+/*== DEFINICIE FUNKCII ==
+ *=======================
+ */
 
 void vytvorTelo( uint32_t * paket, const uint8_t * addr )
 {
@@ -194,7 +190,7 @@ void vytvorTelo( uint32_t * paket, const uint8_t * addr )
   
   // Spocitanie parity pre masku 01111111111110000000000000
   // Zapise na 25. bit 1 ak je parna parita
-  *paket = *paket | ((uint32_t)( ~countParity( *paket & 0x01FFE000 ) & 1 ) << 25 );
+  *paket = *paket | (( uint32_t )( ~countParity( *paket & 0x01FFE000 ) & 1 ) << 25 );
   
   #if DEBUG == 1
     Serial.println( F( "PAKET: " ));
@@ -208,7 +204,7 @@ uint8_t countParity( uint32_t n )
 {
   n = (( n & 0xAAAAAAAA ) >> 1 ) + ( n & 0x55555555 );
   n = (( n & 0x30C30C30 ) >> 4 ) + (( n & 0x0C30C30C ) >> 2 ) + ( n & 0xC30C30C3 );
-  return (n % 63);
+  return ( n % 63 );
 }
 
 void posliKod( uint32_t *paket, uint8_t vystup )
