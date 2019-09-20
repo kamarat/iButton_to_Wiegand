6004fd08c7f471a432cd3f9fb0014aa1530139ce
kamarat <kamarat@centrum.cz>
2019-09-20 13:34:46 +0200

    Úprava informáciíí o verzii.

--- iButton_to_Wiegand.ino
+++ iButton_to_Wiegand.ino
@@ -6,17 +6,11 @@
  * Program nacita ID z iButtona a vytvori signal v protokole Wiegand
  *
  * @author: kamarat
- * @date:   marec 2016 - maj 2017
+ * @date:   marec 2016 - september 2019
  *
+ * @version: [3.0.2] - 2019-09-20
  * @version: [3.0.1] - 2017-05-12
- *    - Zmena zapojenia pinov
- *    - Skratenie doby spanku
- *
  * @version: [3.0.0] - 2017-03-27
- *    - Doplnenie protokolu Wiegand 34
- *    - Spracovanie protokolu v oddelených súboroch
- *    - Úprava identifikácie na základe tzv. Family Code
- *
  * @version: [2.1.0] - 2017-03-24
  * @version: [2.0.0] - 2017-03
  *
